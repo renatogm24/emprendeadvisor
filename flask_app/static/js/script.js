91f70cf3709b7f648e857b93b06472b481e24421
renatogm24 <renato.garay@pucp.pe>
2022-02-02 18:35:44 -0500

    url20

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -17,6 +17,7 @@ loginForm.addEventListener("submit", async (e) => {
   const response = await fetch("https://www.emprendeadvisor.com/login", {
     method: "POST",
     body: form,
+    credentials: "same-origin",
   });
   const data = await response.json();
   if ("error" in data) {
