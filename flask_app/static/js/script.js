0ac5990fdff639105c7d8d8927bdf7588928ed20
renatogm24 <renato.garay@pucp.pe>
2022-02-01 20:15:15 -0500

    with public ip

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -14,7 +14,7 @@ loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const form = new FormData(loginForm);
   form.append("pathname", window.location.pathname);
-  const response = await fetch("http://127.0.0.1/login", {
+  const response = await fetch("http://18.205.29.39/login", {
     method: "POST",
     body: form,
   });
