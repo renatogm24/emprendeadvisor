9f8686e7462bc351646876792ebfe18983e55784
renatogm24 <renato.garay@pucp.pe>
2022-02-03 10:52:33 -0500

    url40

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,8 +5,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, support_credentials=True)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
+#CORS(app, support_credentials=True)
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.secret_key = os.environ.get("secret_key")
 
