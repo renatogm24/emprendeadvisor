697690b32714fcf2743db108f8253de75f2622e6
renatogm24 <renato.garay@pucp.pe>
2022-02-03 10:23:02 -0500

    url37

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -17,7 +17,7 @@ loginForm.addEventListener("submit", async (e) => {
   const response = await fetch("https://emprendeadvisor.com/login", {
     method: "POST",
     body: form,
-    method: "cors",
+    mode: "cors",
   });
   const data = await response.json();
   if ("error" in data) {
