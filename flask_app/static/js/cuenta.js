3949c6f2e301fa66586dbb370ed8ccaef8064c59
renatogm24 <renato.garay@pucp.pe>
2022-02-03 11:09:52 -0500

    url43

--- flask_app/static/js/cuenta.js
+++ flask_app/static/js/cuenta.js
@@ -32,7 +32,9 @@ for (const option of optionsMenu) {
       profileForm = info.querySelector(".profileForm");
 
       if (optionTxt === "Mi Perfil") {
-        const response = await fetch("http://18.205.29.39:5000/getUserSession");
+        const response = await fetch(
+          "https://emprendeadvisor.com/getUserSession"
+        );
         const data = await response.json();
 
         profileForm.innerHTML = "";
@@ -385,7 +387,7 @@ async function tableCreate(action, element, headers, path, limit) {
     profileForm.appendChild(button);
 
     const offset = 0;
-    const link = `http://18.205.29.39:5000${path}/${limit}/${offset}`;
+    const link = `https://emprendeadvisor.com${path}/${limit}/${offset}`;
     console.log(link);
 
     const response = await fetch(link);
@@ -440,7 +442,7 @@ async function tableCreate(action, element, headers, path, limit) {
       offset = 0;
     }
     const response = await fetch(
-      `http://18.205.29.39:5000${path}/${limit}/${offset}`
+      `https://emprendeadvisor.com${path}/${limit}/${offset}`
     );
     const data = await response.json();
     if ("users" in data) {
@@ -458,7 +460,7 @@ async function tableCreate(action, element, headers, path, limit) {
   if (action === "search") {
     const offset = 0;
     const response = await fetch(
-      `http://18.205.29.39:5000${path}/${limit}/${offset}`
+      `https://emprendeadvisor.com${path}/${limit}/${offset}`
     );
     const data = await response.json();
     if ("users" in data) {
@@ -580,7 +582,7 @@ async function actionElement(e, type, url) {
     let data = {};
 
     if (action === "update") {
-      response = await fetch(`http://18.205.29.39:5000${url}`);
+      response = await fetch(`https://emprendeadvisor.com${url}`);
       data = await response.json();
     }
     let result = "";
@@ -641,7 +643,7 @@ async function actionElement(e, type, url) {
   }
 
   if (action === "delete") {
-    const response = await fetch(`http://18.205.29.39:5000${url}`);
+    const response = await fetch(`https://emprendeadvisor.com${url}`);
     const data = await response.json();
     let result = "";
     if (dataType in data) {
@@ -673,7 +675,7 @@ async function updateForm(event, url) {
   success.innerText = "";
 
   const form = new FormData(event.target);
-  const response = await fetch(`http://18.205.29.39:5000${url}`, {
+  const response = await fetch(`https://emprendeadvisor.com${url}`, {
     method: "POST",
     body: form,
   });
@@ -709,7 +711,7 @@ async function updateProfile(event) {
   success.innerText = "";
 
   const form = new FormData(event.target);
-  const response = await fetch("http://18.205.29.39:5000/updateProfile", {
+  const response = await fetch("https://emprendeadvisor.com/updateProfile", {
     method: "POST",
     body: form,
   });
@@ -753,7 +755,7 @@ async function updatePassword(event) {
     return;
   }
 
-  const response = await fetch("http://18.205.29.39:5000/updatePassword", {
+  const response = await fetch("https://emprendeadvisor.com/updatePassword", {
     method: "POST",
     body: form,
   });
