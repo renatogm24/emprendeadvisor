cea80aa8243955278606b53c6c60eb6c9d76ea3f
renatogm24 <renato.garay@pucp.pe>
2022-02-05 21:06:11 -0500

    redis 5 and instagrapi 3

--- flask_app/controllers/emprendimientos.py
+++ flask_app/controllers/emprendimientos.py
@@ -67,6 +67,9 @@ def search(igusername):
   emprendimientoSearch = emprendimiento.Emprendimiento.search_by_username({"username":igusername})
   if not emprendimientoSearch:
     emprendAux = emprendimiento.Emprendimiento(getDataInstagrapi(igusername))
+    arr = emprendAux["profile_pic_url_hd"].split("?")
+    emprendAux["url_p1"] = arr[0]
+    emprendAux["url_p2"] = arr[1]
   return render_template("emprendimiento.html",emprendimiento=emprendAux)
 
 @app.route('/img/<path:url>&<params>')
